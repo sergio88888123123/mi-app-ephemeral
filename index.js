6c25fe94ccfae7eb47dc3b95b3d6ea00a563c6b1
sergio88888123123 <137866905+sergio88888123123@users.noreply.github.com>
2026-03-20 01:11:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Ambiente efímero en Heroku</h1>
-    <p>Aplicación de prueba desplegada con Node.js y GitHub.</p>
+    <h1>Probando en Heroku</h1>
+    <p> Si.</p>
     <p>Este entorno se usa para validar cambios antes de integrarlos al proyecto principal.</p>
   `);
 });
